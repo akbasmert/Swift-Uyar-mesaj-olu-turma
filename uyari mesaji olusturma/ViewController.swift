4bb648b3dec89bd69ae10014b78025b44fa285bd
mert <mert.ma346@gmail.com>
2022-08-20 12:22:04 +0300

    kullanıcı ekranı tasarlandı

--- uyari mesaji olusturma/ViewController.swift
+++ uyari mesaji olusturma/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var password2TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func signUpTiklandi(_ sender: Any) {
+    }
+    
 }
 
